e3263050a51b4a0934282f56e6a300226ab7722b
FemTubey <161897656+FemTubey@users.noreply.github.com>
2026-02-20 04:44:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     siteTitle: "FemTubey Video Vault",
     discordClientId: "1397766534876762143",
-    redirectUri: "https://discord.com/oauth2/authorize?client_id=1397766534876762143&response_type=code&redirect_uri=https%3A%2F%2Ffemtubey.github.io%2Fyep%2F&scope=identify+guilds",
+    redirectUri: "https://femtubey.github.io/yep/",
     videos: [
         {
             title: "First Video",
