7320e673da674f0bc6f87d18e92d666408ad28db
FemTubey <161897656+FemTubey@users.noreply.github.com>
2026-02-20 04:59:19 -0500

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,6 @@ const CONFIG = {
             url: "https://raw.githubusercontent.com/FemTubey/yep/main/1.mp4",
             thumbnail: "https://via.placeholder.com/160x90"
         }
+        // You can add more video objects here
     ]
 };
